dfd128c66f60a408e6c8d6a80909c10378bfca56
Ivo Correa <ivocorrea2005@email.com>
2026-06-30 00:08:41 -0500

    Preparar despliegue optimizado: subir build local

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-﻿import { Module } from '@nestjs/common';
+﻿import { Module, Global } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { NetworkModule } from './network/network.module';
@@ -6,34 +6,24 @@ import { User } from './users/entities/user.entity';
 import { AuthController } from './auth.controller';
 import { RegisterController } from './auth/register.controller';
 
+@Global() // Hacemos el módulo global
 @Module({
   imports: [
-    // 1. Configuración centralizada
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
-      entities: [User, __dirname + '/**/*.entity{.ts,.js}'], // Asegura que encuentre todas las entidades
+      entities: [User, __dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false,
       logging: true,
-      ssl: {
-        rejectUnauthorized: false,
-      },
-      extra: {
-        connectionTimeoutMillis: 10000,
-      },
+      ssl: { rejectUnauthorized: false },
+      extra: { connectionTimeoutMillis: 10000 },
       autoLoadEntities: true, 
     }),
-    
-    // 2. Eliminamos forFeature([User]) de aquí, 
-    // debe estar solo en los módulos hijos (UsersModule)
-    
     UsersModule,
     NetworkModule,
   ],
-  controllers: [
-    AuthController, 
-    RegisterController
-  ],
+  exports: [TypeOrmModule], // Exportamos explícitamente
+  controllers: [AuthController, RegisterController],
   providers: [],
 })
 export class AppModule {}
