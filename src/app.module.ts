5422de6c670d884f1cfa51a376e75f616a4e88df
Ivo Correa <ivocorrea2005@email.com>
2026-06-29 23:11:28 -0500

    Mejora manejo de errores inicio

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,21 +8,21 @@ import { RegisterController } from './auth/register.controller';
 
 @Module({
   imports: [
+    /*
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
       entities: [User],
-      synchronize: false, // Obligatorio en producción
+      synchronize: false,
       logging: true,
-      // Configuración consolidada para Render + Neon
       ssl: {
         rejectUnauthorized: false,
       },
       extra: {
-        // Aumentamos el tiempo de espera para evitar timeouts en el arranque
         connectionTimeoutMillis: 10000,
       },
     }),
+    */
     TypeOrmModule.forFeature([User]), 
     UsersModule,
     NetworkModule,
